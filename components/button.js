c8d3fb74873f5e52bc881dbabe6738f2ac0f7fe7
Mustafa Erdogan <c.mustafaerdoann@hotmail.com>
2020-07-31 01:00:34 +0300

    Add files via upload

---
+++ components/button.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import styles from  './button.module.css'
+function Button({ children }){
+ 
+
+return <button type="button" className={styles.button}>Children </button>
+
+
+
+}
+
+
+
+export default Button
